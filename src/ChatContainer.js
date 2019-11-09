0ab87cbb8250557ba965fe435ad67abf26037226
Vladyslav Khrystych <vladyslavkhrystych@gmail.com>
2019-11-09 16:54:17 +0200

    Move fake data to mock-data.js

--- src/ChatContainer.js
+++ src/ChatContainer.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 
 import { withStyles } from "@material-ui/styles";
 
+import { contacts } from "./mock-data";
+
 import {
   List,
   Badge,
@@ -59,114 +61,6 @@ const styles = {
     alignSelf: "flex-start"
   }
 };
-
-const getAvatar = text => {
-  return `https://api.adorable.io/avatars/50/${Math.floor(
-    Math.random() * Math.floor(100)
-  )}.png`;
-};
-
-const contacts = [
-  {
-    avatar: getAvatar(),
-    name: "Vladyslav Khrystych",
-    msgCount: 0,
-    msg:
-      "Here’s how to follow the summit and weekend festivities, featuring CEOs, actors, authors, scientists, and engineers."
-  },
-  {
-    avatar: getAvatar(),
-    name: "Frol Enazt",
-    msgCount: 0,
-    msg:
-      "We’ll have climate activists and CEOs, actors and authors, scientists and engineers—plus a guy who can teach you to make the world's best paper airplane."
-  },
-  {
-    avatar: getAvatar(),
-    name: "Anton Syrovatka",
-    msgCount: 3,
-    msg:
-      "In the meantime, you can catch up on everything that happened at last year’s inaugural event"
-  },
-  {
-    avatar: getAvatar(),
-    name: "Anastasiya Kurhan",
-    msgCount: 5,
-    msg: "Below is the full schedule of Friday’s summit."
-  },
-  {
-    avatar: getAvatar(),
-    name: "Artyom Sazonov",
-    msgCount: 1,
-    msg: "Jeff Weiner, CEO, LinkedIn, with Nicholas Thompson, WIRED"
-  },
-  {
-    avatar: getAvatar(),
-    name: "Vladyslav Khrystych",
-    msgCount: 0,
-    msg:
-      "Here’s how to follow the summit and weekend festivities, featuring CEOs, actors, authors, scientists, and engineers."
-  },
-  {
-    avatar: getAvatar(),
-    name: "Frol Enazt",
-    msgCount: 0,
-    msg:
-      "We’ll have climate activists and CEOs, actors and authors, scientists and engineers—plus a guy who can teach you to make the world's best paper airplane."
-  },
-  {
-    avatar: getAvatar(),
-    name: "Anton Syrovatka",
-    msgCount: 3,
-    msg:
-      "In the meantime, you can catch up on everything that happened at last year’s inaugural event"
-  },
-  {
-    avatar: getAvatar(),
-    name: "Anastasiya Kurhan",
-    msgCount: 5,
-    msg: "Below is the full schedule of Friday’s summit."
-  },
-  {
-    avatar: getAvatar(),
-    name: "Artyom Sazonov",
-    msgCount: 1,
-    msg: "Jeff Weiner, CEO, LinkedIn, with Nicholas Thompson, WIRED"
-  },
-  {
-    avatar: getAvatar(),
-    name: "Vladyslav Khrystych",
-    msgCount: 0,
-    msg:
-      "Here’s how to follow the summit and weekend festivities, featuring CEOs, actors, authors, scientists, and engineers."
-  },
-  {
-    avatar: getAvatar(),
-    name: "Frol Enazt",
-    msgCount: 0,
-    msg:
-      "We’ll have climate activists and CEOs, actors and authors, scientists and engineers—plus a guy who can teach you to make the world's best paper airplane."
-  },
-  {
-    avatar: getAvatar(),
-    name: "Anton Syrovatka",
-    msgCount: 3,
-    msg:
-      "In the meantime, you can catch up on everything that happened at last year’s inaugural event"
-  },
-  {
-    avatar: getAvatar(),
-    name: "Anastasiya Kurhan",
-    msgCount: 5,
-    msg: "Below is the full schedule of Friday’s summit."
-  },
-  {
-    avatar: getAvatar(),
-    name: "Artyom Sazonov",
-    msgCount: 1,
-    msg: "Jeff Weiner, CEO, LinkedIn, with Nicholas Thompson, WIRED"
-  }
-];
 
 const Contact = ({ contact, contactIndex, classes, isSelected, onClick }) => {
   return (
